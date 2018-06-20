3208c3402bb074f054e5f1b57a1d10275bf98bd0
Monmonstar <louisgreenland1@hotmail.com>
2018-06-20 15:59:41 +0100

    erg

---
+++ Entities/Assets/scripts/obj_moving.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obj_moving : MonoBehaviour {
+
+    public bool moving;
+    public Transform[] waypoints;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    void StartMoving(){
+        
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
